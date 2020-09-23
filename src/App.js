49d570729e54d571df87c6023cb861e8fbe23e7f
juanmana <juanmana0402@gmail.com>
2020-09-23 19:12:53 +0200

    prueba

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
-function App() {
+export const App = () => {
+  const [data, setData] = useState([]);
+  const [user, setUser] = useState({
+    id: '',
+    email: '',
+    first_name:'',
+    last_name:''
+
+
+  });
+
+  useEffect(() => {
+    axios
+
+      .get(`https://reqres.in/api/users`)
+      .then((res) => {
+        console.log(res.data.data);
+        setData(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+
+
+
+
+
+
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <ul>
+        {data.map((user) => (
+          <div  key={user.id}>
+            
+            <li>{user.email}</li>
+
+            <li>{user.first_name}</li>
+
+            <li>{user.last_name}</li>
+
+             <button>Delete</button>
+             <button>Edit</button>
+
+             <hr/>
+          </div>
+        ))}
+      </ul>
     </div>
   );
-}
-
-export default App;
+};
